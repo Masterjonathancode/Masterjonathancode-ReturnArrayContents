498c50a21631ac8728e3a03266875947a9a5ceba
Masterjonathancode <jon.a.mammen@gmail.com>
2023-03-22 19:32:07 +0000

    Completed Return Array Contents Coding Challenge

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -29,7 +29,108 @@ public class Lab {
      * @param nums the array to be manipulated.
      * @return a string representation of all values of nums, with the values separated by spaces.
      */
+  
+     // input primitive integer array
+    //int[] intArray = {};
+    //int[] nums = {}; 
+     //String[] strArray = new String[intArray.length];
+     //String chr; 
+    
     public String arrayToString(int[] nums){
-        return "";
-    }
+        //int[] intArray = {};
+     //int array
+ //int[] intNumbers = new int[]{1, 2, 3, 4, 5};
+    //nums = new int[]{1,2,3,4,5}; 
+    //nums = new int[]{};
+ /*
+ * First approach is to loop through all elements of an int array 
+ * and append them to StringBuffer object one by one. At the end,
+ * use toString method to convert it to String.
+ */
+//create new StringBuffer object
+StringBuffer sbfNumbers = new StringBuffer();
+//define the separator you want in the string. This example uses space.
+String strSeparator = " ";
+if(nums.length > 0)
+{
+    //we do not want leading space for first element
+ sbfNumbers.append(nums[0]);
+ /*
+ * Loop through the elements of an int array. Please
+ * note that loop starts from 1 not from 0 because we 
+ * already appended the first element without leading space.s	
+ */
+        for(int i = 1; i < nums.length; i++){
+          
+            sbfNumbers.append(strSeparator).append(nums[i]);
+        
+         }
+
 }
+System.out.println("int array converted to String using for loop");
+//finally convert StringBuffer to String using toString method
+System.out.println(sbfNumbers.toString());
+/*
+ * Second options is to use Arrays class as given below.
+ * Use Arrays.toString method to convert int array to String.
+ * 
+ * However, it will return String like [1, 2, 3, 4, 5]
+ */
+String strNumbers = Arrays.toString(nums);
+System.out.println("String generated from Arrays.toString method: " + strNumbers);
+//you can use replaceAll method to replace brackets and commas
+strNumbers = strNumbers.replaceAll(", ", strSeparator).replace("[", "").replace("]", "");
+System.out.println("Final String: " + strNumbers);
+return strNumbers; 
+//for (int i = 0; i < nums.length; i++) {
+        
+       
+       // strArray[i] = String.valueOf(intArray[i]);
+       // nums[i] = Integer.parseInt(str[i]);
+        //String[] strArray = new String[nums.length];
+        //nums = Arrays.copyOf(nums, nums.length);
+        //strArray[i] = String.valueOf(nums[i]);
+        //String str = strArray[i]; 
+        //strArray[i] = String.valueOf(nums[i]);
+        // Program to convert primitive integer array to string array in 
+        //strArray[i] = String.valueOf(nums);
+         //Arrays.toString(nums);
+        //System.out.println(Arrays.toString(nums));
+         //Arrays.toString(nums);  
+
+
+//String str[] = {"apple", "banana", "cherry"};//
+//String arrNew[] = {"apple", "banana", "cherry"};//
+//int[] nums = {};//
+//String [] str = {"1", "345", "437", "894"};//
+//int size = str.length;//
+//int [] nums = new int [size];//
+//String [] strArray = new String[nums.length];//
+//nums = Arrays.copyOf(intArray, nums.length -1);//
+//intArray[i] = nums[i];//
+//chr = intArray.toString(); //
+//String  str = strArray[i]; //
+//chr = Arrays.toString(nums); //
+//strArray[i] = String.valueOf(nums);//
+//strArray[i] = String.valueOf(intArray[i]);//
+//nums[i] = Integer.parseInt(str[i]);//
+//String[] strArray = new String[nums.length];//
+//nums = Arrays.copyOf(nums, nums.length-1);//
+//strArray[i] = String.valueOf(nums[i]);//
+//String str = strArray[i]; //
+//strArray[i] = String.valueOf(nums[i]);//
+// Program to convert primitive integer array to string array in 
+//strArray[i] = String.valueOf(nums);//
+//Arrays.toString(nums);//
+//System.out.println(Arrays.toString(nums));//
+//Arrays.toString(nums);  //
+// return chr; //
+ }//
+ ///return Arrays.toString(nums);//
+ //return chr; 
+ //return chr;
+
+
+         }
+       
+  
